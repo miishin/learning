02df221433ebad51e2940c0e2d7cbbf42e08483c
miishin <miishing@gmail.com>
2021-02-15 00:16:19 -0500

    Update codewars.rb

--- learning_ruby/codewars.rb
+++ learning_ruby/codewars.rb
@@ -1,3 +1,61 @@
+# This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+# seven(times(five)) # must return 35
+# four(plus(nine)) # must return 13
+# eight(minus(three)) # must return 5
+# six(divided_by(two)) # must return 3
+# Requirements:
+
+# There must be a function for each number from 0 ("zero") to 9 ("nine")
+# There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy (divided_by in Ruby and Python)
+# Each calculation consist of exactly one operation and two numbers
+# The most outer function represents the left operand, the most inner function represents the right operand
+# Division should be integer division. For example, this should return 2, not 2.666666...:
+
+def zero(expr=nil)
+  expr.nil? ? 0 : expr.call(0)
+end
+def one(expr=nil)
+  expr.nil? ? 1 : expr.call(1)
+end
+def two(expr=nil)
+  expr.nil? ? 2 : expr.call(2)
+end
+def three(expr=nil)
+  expr.nil? ? 3 : expr.call(3)
+end
+def four(expr=nil)
+  expr.nil? ? 4 : expr.call(4)
+end
+def five(expr = nil)
+  expr.nil? ? 5 : expr.call(5)
+end
+def six(expr=nil)
+  expr.nil? ? 6 : expr.call(6)
+end
+def seven(expr=nil)
+  expr.nil? ? 7 : expr.call(7)
+end
+def eight(expr=nil)
+  expr.nil? ? 8 : expr.call(8)
+end
+def nine(expr=nil)
+  expr.nil? ? 9 : expr.call(9)
+end
+def plus(n)
+  -> (x) { x + n }
+end
+def minus(n)
+  -> (x) { x - n }
+end
+def times(n)
+  -> (x) { x * n }
+end
+def divided_by(n)
+  -> (x) { x / n}
+end
+
+# I thought about church encodings at first but the operators only take in one arg. This helped me learn how to do lambdas in Ruby.
+
 # A pangram is a sentence that contains every single letter of the alphabet at least once. 
 # For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 # Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
