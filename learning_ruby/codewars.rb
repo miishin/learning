1b0b764b07f1a7095d18b68830c1f9df4c91e16e
miishin <miishing@gmail.com>
2021-02-19 16:36:15 -0500

    Update codewars.rb

--- learning_ruby/codewars.rb
+++ learning_ruby/codewars.rb
@@ -1,3 +1,13 @@
+# Write a function called sumIntervals/sum_intervals() that accepts an array of intervals, and returns the sum of all the interval lengths. 
+# Overlapping intervals should only be counted once.
+# Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. 
+# Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+# https://www.codewars.com/kata/52b7ed099cdc285c300001cd/
+
+# found in intervals.rb
+
+# Still a bit confused on when "for" is ever used - seems useless when each exists.
+
 # Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
 # which is the number of times you must multiply the digits in num until you reach a single digit.
 
